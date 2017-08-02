249e1da4a6556f3cced8d5b51adc29d53ddd8e55
Rickard Sunden <rickard.sunden@outlook.com>
2017-08-02 15:46:16 -0400

    use scss and bulma

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './css/bulma.css'
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
