59b3fa2800885c9d43463405b8a6e77c3ae704e5
Rickard Sunden <rickard.sunden@outlook.com>
2017-08-07 20:54:01 -0400

    add cookie support

--- src/helpers/axe.js
+++ src/helpers/axe.js
@@ -1,11 +1,12 @@
 import axios from 'axios';
+import cookies from './cookies.js';
 
 class Axe {
   constructor(){
     this.request = axios.create({
       baseURL: 'https://api.example.com'
     });
-    this.loggedIn = true;
+    this.loggedIn = cookies.get('loggedIn') || false;
   }
 
 }
